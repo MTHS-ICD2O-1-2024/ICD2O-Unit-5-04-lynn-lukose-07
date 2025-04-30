77a05abcd4fbc286588f0123f4e1f9df2771d386
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-04-30 17:12:01 +0000

    edited stuff

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lynn Lukose
+// Created on: apr 2025
 // This file contains the JS functions for index.html
+function check() {
+  // input
+  const day = document.getElementById("day").value;
+  const userAge = parseInt(document.getElementById("user-age").value);
+
+  // process + output
+  if (day === "Tuesday" || day === "Thursday" || (userAge > 12 && userAge < 21)) {
+    document.getElementById("result").innerHTML =
+      "You get a discount for the museum!";
+  } else {
+    document.getElementById("result").innerHTML =
+      "You must pay the regular price.";
+  }
+}
