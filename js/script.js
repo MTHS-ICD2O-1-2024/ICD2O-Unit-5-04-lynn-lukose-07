e72071142bc30db9cd6d65d33b6ffe99bc3d8d43
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-05-04 02:02:34 +0000

    edit js

--- js/script.js
+++ js/script.js
@@ -8,15 +8,19 @@
 
 function check() {
   // input
-  const day = document.getElementById("day").value;
-  const userAge = parseInt(document.getElementById("user-age").value);
+  const day = document.getElementById("day").value
+  const userAge = parseInt(document.getElementById("user-age").value)
 
   // process + output
-  if (day === "Tuesday" || day === "Thursday" || (userAge > 12 && userAge < 21)) {
+  if (
+    day === "Tuesday" ||
+    day === "Thursday" ||
+    (userAge > 12 && userAge < 21)
+  ) {
     document.getElementById("answer").innerHTML =
-      'You get a discount for the museum!';
+      "You get a discount for the museum!"
   } else {
     document.getElementById("answer").innerHTML =
-      'You must pay the regular price.';
+      "You must pay the regular price."
   }
 }
